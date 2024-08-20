6fca7b715c4b87fad49445747f8011ba46e7cb38
swaranjali01 <swaragawande26@gmail.com>
2024-08-20 12:02:39 +0530

    day 14 progress

--- day-14/swara.js
+++ day-14/swara.js
@@ -1,12 +1,35 @@
 let gameSeq=[];
 let userSeq=[];
 
+let btns = [ " yellow", "red", "purple", "green"];
+
 let started = false;
 let level =0;
+
+let h2 = document.querySelector("h2");
 
 document.addEventListener("keypress", function () {
     if (started=false) {
         console.log( "game is started" );
         started = true;
+           
+        levelUp();
     }
-})
+});
+
+function btnFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    },250);
+}
+
+function levelUp() {
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * 3);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    brnFlash();
+}
